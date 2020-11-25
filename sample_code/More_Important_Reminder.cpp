cb01d49ed3546b0396c69739f2518c88321ddb48
PrincessLunaZ <bowlingpinsystem31+1@gmail.com>
2020-11-25 00:23:19 -0800

    Fooling with C++

---
+++ sample_code/More_Important_Reminder.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // PLEASE DO THIS OWEN!!! PLEASE!!!!
+    cout << "I miss Samantha.\n"<<"I love her so much.\n"<<"Please tell her to text me.\n"<<"I want to see her in person."
+    return 0;
+}
---
+++ sample_code/Reminder.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Attend the module or else you get an unexcused absence" << endl;
+    cout << "Or I will wake you up at 5AM tomorrow.";
+    return 0;
+}
