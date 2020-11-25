efb53398cd0548d2200a00a7f42dc1ad1d0f5524
PrincessLunaZ <bowlingpinsystem31+1@gmail.com>
2020-11-25 00:26:24 -0800

    Add a ; at the end

--- sample_code/More_Important_Reminder.cpp
+++ sample_code/More_Important_Reminder.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main()
 {
     // PLEASE DO THIS OWEN!!! PLEASE!!!!
-    cout << "I miss Samantha.\n"<<"I love her so much.\n"<<"Please tell her to text me.\n"<<"I want to see her in person."
+    cout << "I miss Samantha.\n"<<"I love her so much.\n"<<"Please tell her to text me.\n"<<"I want to see her in person.";
     return 0;
 }
