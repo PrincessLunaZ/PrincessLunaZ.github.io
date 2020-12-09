4068d4e19b86b5ab3affaba2860805a0b9d6942c
PrincessLunaZ <74730757+PrincessLunaZ@users.noreply.github.com>
2020-12-08 18:04:13 -0800

    Delete More_Important_Reminder.cpp

--- sample_code/More_Important_Reminder.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    // PLEASE DO THIS OWEN!!! PLEASE!!!!
-    cout << "I miss Samantha.\n"<<"I love her so much.\n"<<"Please tell her to text me.\n"<<"I want to see her in person.";
-    return 0;
-}
