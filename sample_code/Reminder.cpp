6ba8e89e9beda3a5bca55a8f4fbce5a9025fa420
PrincessLunaZ <74730757+PrincessLunaZ@users.noreply.github.com>
2020-12-08 18:04:39 -0800

    Delete Reminder.cpp

--- sample_code/Reminder.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "Attend the module or else you get an unexcused absence" << endl;
-    cout << "Or I will wake you up at 5AM tomorrow.";
-    return 0;
-}
