bd718376094c288b8bb63d7962ef03bdf39a7e0c
PrincessLunaZ <bowlingpinsystem31@Gmail.com>
2020-12-11 15:12:49 -0800

    test

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
 function myFunction() {
     alert("You clicked submit! Eventually, this will do something else.")
 }
+function getPassword() {
+	var s ="";
+
+while (s!= "test")
+{
+s=prompt("please enter your password");
+if (s=="test")
+{
+window.location.href="/files"; //page to redirect if password entered is correct
+
+}
+else
+{
+alert("Incorrect password-Try again");
+
+}
+}
+
+}
