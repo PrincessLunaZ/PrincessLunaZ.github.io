4bde48a59cb55fb76989b0504c644dc3b45b7f10
PrincessLunaZ <74730757+PrincessLunaZ@users.noreply.github.com>
2021-01-14 23:30:33 -0800

    Create starter-kit-for-termux.sh

---
+++ starter-kit-for-termux.sh
@@ -0,0 +1,5 @@
+apt install python nodejs wget curl
+wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
+chmod +x metasploit.sh
+npm install discord.js
+pip install discord.py
