0245963d02f49f29ac3073eed9929cec7fc0ca56
PrincessLunaZ <74730757+PrincessLunaZ@users.noreply.github.com>
2021-03-02 10:03:59 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 function myFunction() {
     alert("You clicked submit! Eventually, this will do something else.")
 }
-function getPassword() {
-	var s ="";
-
-while (s!= "PrincessRarity45!")
-{
-s=prompt("please enter your password");
-if (s=="PrincessRarity45!")
-{
-window.location.href="/files/showimages.html"; //page to redirect if password entered is correct
-
-}
-else
-{
-alert("Incorrect password-Try again");
-
-}
-}
-
-}
+//function getPassword() {
+//	var s ="";
+//
+//while (s!= "PrincessRarity45!")
+//{
+//s=prompt("please enter your password");
+//if (s=="PrincessRarity45!")
+//{
+//window.location.href="/files/showimages.html"; //page to redirect if password entered is correct
+//
+//}
+//else
+//{
+//alert("Incorrect password-Try again");
+//
+//}
+//}
+//
+//}
