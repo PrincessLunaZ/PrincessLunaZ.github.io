d3cb01795f52244ba2d25b2153772dff257b4291
PrincessLunaZ <74730757+PrincessLunaZ@users.noreply.github.com>
2021-03-28 12:11:49 -0700

    Update musicbot.sh

--- termux_sh/musicbot.sh
+++ termux_sh/musicbot.sh
@@ -3,8 +3,8 @@ apt-get update -y
 apt-get upgrade -y
 
 # Install dependencies
-apt-get install git libopus-dev libffi-dev libsodium-dev ffmpeg -y
-apt-get install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl -y
+apt-get install git libopus libffi libsodium ffmpeg -y
+apt-get install build-essential libncursesw5 libgdbm libc6 zlib1g libsqlite3 tk libssl openssl -y
 
 # If using Debian Stretch or lower, you need to install Python too using...
 apt-get install python3.5 python3-pip -y
