b3113035f9b9ec8c351f4770fd1aafb4bac43444
PrincessLunaZ <74730757+PrincessLunaZ@users.noreply.github.com>
2021-06-05 09:13:02 -0700

    Delete starter-kit-for-termux.sh

--- starter-kit-for-termux.sh
+++
@@ -1,5 +0,0 @@
-apt install python nodejs wget curl
-wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
-chmod +x metasploit.sh
-npm install discord.js
-pip install discord.py
