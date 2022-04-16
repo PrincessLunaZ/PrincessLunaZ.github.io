e71616f6e6b5d1424e28729f37fef620a6eebee1
PrincessLunaZ <74730757+PrincessLunaZ@users.noreply.github.com>
2022-04-16 14:10:26 -0700

    Create grade_calc.cpp

---
+++ c++_code/grade_calc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// Insert passing grade here
+int main()
+{
+  int mark = 50;
+  // Failing grade here
+  if (mark < 50) {
+    cout << "FAIL!!!!" << endl;
+  }
+  else {
+    cout << "WINNER!!!!" << endl;
+  }
+  return 0;
+}
